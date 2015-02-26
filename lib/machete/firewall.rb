a98baef12eb184e4659bce2215f07ef9f5e1bb76
Anthony Emengo and Sai To Yeung <pair+aemengo+syeung@pivotal.io>
2015-02-26 15:09:05 -0500

    Turns on firewall for trusty

--- lib/machete/firewall.rb
+++ lib/machete/firewall.rb
@@ -80,10 +80,12 @@ module Machete
       end
 
       def cf_subnet
+        '10.244.1.0/24'
         '10.244.0.0/24'
       end
 
       def mac_subnet
+        '192.168.200.0/24'
         '192.168.50.0/24'
       end
 
